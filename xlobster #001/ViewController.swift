d95bab081966e97df7c66ae2c3f92aac2fb0bf54
Ming <toleremy@gmail.com>
2022-07-18 01:29:27 +0800

    Initial Commit

---
+++ xlobster #001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xlobster #001
+//
+//  Created by SamY on 2022/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
